3aee782955fd1bb79d24048295787460fdba6ecb
Taylor Blau <me@ttaylorr.com>
2018-04-21 10:16:20 -0700

    mutt.rb,patch: add patch to retain message ID

---
+++ README.md
@@ -0,0 +1,12 @@
+# homebrew-mutt
+
+Homebrew tap for install `mutt(1)` with the patches in `patch`.
+
+## Patches
+
+- `with-retain-messageid`: apply patch `0001` to retain the `Message-ID:`
+  header when resending messages.
+
+## Copyright
+
+Copyright © 2018-, Taylor Blau. Released under the MIT License.
---
+++ mutt.rb
@@ -0,0 +1,102 @@
+# Note: Mutt has a large number of non-upstream patches available for
+# it, some of which conflict with each other. These patches are also
+# not kept up-to-date when new versions of mutt (occasionally) come
+# out.
+#
+# To reduce Homebrew's maintenance burden, patches are not accepted
+# for this formula. The NeoMutt project has a Homebrew tap for their
+# patched version of Mutt: https://github.com/neomutt/homebrew-neomutt
+
+class Mutt < Formula
+  desc "Mongrel of mail user agents (part elm, pine, mush, mh, etc.)"
+  homepage "http://www.mutt.org/"
+  url "https://bitbucket.org/mutt/mutt/downloads/mutt-1.9.5.tar.gz"
+  mirror "http://ftp.mutt.org/pub/mutt/mutt-1.9.5.tar.gz"
+  sha256 "bf617e64ae4e08a998bef8e42a965a211587e051f1437a3a4884b351a9385753"
+
+  bottle do
+    sha256 "34f2d81ebe67fe3049a22a9ee31111575c708db2307b974bfaa046af4a2bcbdf" => :high_sierra
+    sha256 "96e72bfbfcc89e66037b98cf1d6a262d697e99a9c18a6d3390864a97f91c4593" => :sierra
+    sha256 "062df74d0def4530f280ca9a9affe82d819ba73f076de164262b152ecc83e7ce" => :el_capitan
+  end
+
+  head do
+    url "https://gitlab.com/muttmua/mutt.git"
+
+    resource "html" do
+      url "https://dev.mutt.org/doc/manual.html", :using => :nounzip
+    end
+  end
+
+  option "with-debug", "Build with debug option enabled"
+  option "with-s-lang", "Build against slang instead of ncurses"
+  option "with-retain-messageid", "Build with messageid retaining patch"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "openssl"
+  depends_on "tokyo-cabinet"
+  depends_on "gettext" => :optional
+  depends_on "gpgme" => :optional
+  depends_on "libidn" => :optional
+  depends_on "s-lang" => :optional
+
+  conflicts_with "tin",
+    :because => "both install mmdf.5 and mbox.5 man pages"
+
+  patch do
+    url "https://raw.githubusercontent.com/ttaylorr/homebrew-mutt/master/patch/0001-postpone.c-retain-message_id-when-resending.patch"
+    sha256 "28d754ab1583286c74277c42d5fd702415cc54a425eee7afeadd58cfb8384567"
+  end if build.with? "retain-messageid"
+
+  def install
+    user_admin = Etc.getgrnam("admin").mem.include?(ENV["USER"])
+
+    args = %W[
+      --disable-dependency-tracking
+      --disable-warnings
+      --prefix=#{prefix}
+      --with-ssl=#{Formula["openssl"].opt_prefix}
+      --with-sasl
+      --with-gss
+      --enable-imap
+      --enable-smtp
+      --enable-pop
+      --enable-hcache
+      --with-tokyocabinet
+      --enable-sidebar
+    ]
+
+    # This is just a trick to keep 'make install' from trying
+    # to chgrp the mutt_dotlock file (which we can't do if
+    # we're running as an unprivileged user)
+    args << "--with-homespool=.mbox" unless user_admin
+
+    args << "--disable-nls" if build.without? "gettext"
+    args << "--enable-gpgme" if build.with? "gpgme"
+    args << "--with-slang" if build.with? "s-lang"
+
+    if build.with? "debug"
+      args << "--enable-debug"
+    else
+      args << "--disable-debug"
+    end
+
+    system "./prepare", *args
+    system "make"
+
+    # This permits the `mutt_dotlock` file to be installed under a group
+    # that isn't `mail`.
+    # https://github.com/Homebrew/homebrew/issues/45400
+    if user_admin
+      inreplace "Makefile", /^DOTLOCK_GROUP =.*$/, "DOTLOCK_GROUP = admin"
+    end
+
+    system "make", "install"
+    doc.install resource("html") if build.head?
+  end
+
+  test do
+    system bin/"mutt", "-D"
+  end
+end
